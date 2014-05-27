8a7a27e20e4997e13c780e1f0db1b90109c265d6
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-05-26 22:21:24 -0300

    Implement calculator at angular side

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,4 +2,50 @@
 
 angular.module('hypermathApp')
   .controller('CalculatorController', function ($scope) {
+
+    $scope.result = null;
+    $scope.x= null;
+    $scope.y = null;
+    var next = false;
+
+    $scope.setValue = function (value) {
+      $scope.result = null;
+      if (next) {
+        $scope.y = value;
+        console.log('Y: ' + $scope.y);
+      } else {
+        $scope.x = value;
+        console.log('X: ' + $scope.x);
+      }
+    };
+
+    $scope.setOperation = function (operation) {
+      $scope.operation = operation;
+      next = true;
+      console.log('OPERAÇÃO: ' + $scope.operation);
+    };
+
+    $scope.getResult = function () {
+      switch ($scope.operation) {
+      case '*':
+        $scope.result = $scope.x * $scope.y;
+        break;
+      case '/':
+        $scope.result = $scope.x / $scope.y;
+        break;
+      case '-':
+        $scope.result = $scope.x - $scope.y;
+        break;
+      case '+':
+        $scope.result = $scope.x + $scope.y;
+        break;
+      default:
+
+      }
+      $scope.operation = null;
+      $scope.x         = null;
+      $scope.y         = null;
+      next             = false;
+    };
+
   });
